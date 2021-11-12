f02779a06e80ff78875eb5e4eb7b990e281e81bf
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-12 23:15:39 +0530

    implement intersection

--- DSA450/Arrays/unionIntersection.cpp
+++ DSA450/Arrays/unionIntersection.cpp
@@ -17,14 +17,25 @@ void solve()
     for (int i = 0; i < m; i++)
         cin >> arr2[i];
 
+    //union
     set<int> s;
     for (int i = 0; i <n;i++)
         s.insert(arr1[i]);
     for (int j = 0; j < m;j++)
         s.insert(arr2[j]);
-
+    cout<<"Union:\n"
     for (auto it = s.begin(); it!=s.end();it++)
         cout << *it << " ";
+
+    //intersection
+    
+    set<int> s;
+    for (int i = 0; i <n;i++)
+        s.insert(arr1[i]);
+    cout << "intersection\n";
+    for (int j = 0; j < m;j++)
+        if(s.find(arr2[j])!=s.end())
+            cout << arr2[j] << " ";
 }
 
 int main()
