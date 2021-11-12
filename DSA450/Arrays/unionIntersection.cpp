5587a05189f466213bbec63568dc4a7216bd8012
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-12 20:41:52 +0530

    first draft logic

--- DSA450/Arrays/unionIntersection.cpp
+++ DSA450/Arrays/unionIntersection.cpp
@@ -17,7 +17,27 @@ void solve()
     for (int i = 0; i < m;i++)
         cin >> arr2[i];
 
-    
+    int intersection[min(m, n)];
+    int count = 0;
+
+    for (int i = 0, j = 0; i < n && j < m;)
+    {
+        if(arr1[i]<arr2[j]) 
+        {
+            cout << arr1[i] << " ";
+            j++;
+        }
+        if(arr1[i]>arr2[j])
+        {
+            cout << arr2[j] << " ";
+            i++;
+        }
+        if(arr1[i]==arr2[j])
+            intersection[count++] = arr1[i];
+    }
+
+    for (int i = 0; i < count;i++)
+        cout << "\n"<< intersection[i] << " ";
 }
 
 int main() {
