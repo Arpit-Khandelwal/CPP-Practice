664c09a8fef5bcd4709232bd1da3ff4aa43db8e9
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-12 17:20:49 +0530

    findSmallest function

--- DSA450/Arrays/3.cpp
+++ DSA450/Arrays/3.cpp
@@ -3,6 +3,12 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
+void findSmallest(int arr[],int l, int h, int n)
+{
+    int min = INT_MAX;
+    
+}
+
 void solve()
 {
     int n;
@@ -12,6 +18,7 @@ void solve()
     for (int i = 0; i < n;i++)
         cin >> arr[i];
     cin >> k;
+
     
 }
 
