c21b4fc4d675f47ff2f465c648bbf20d0a5ae8ce
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-13 21:41:37 +0530

    logic 2 implemented

--- DSA450/Strings/51.cpp
+++ DSA450/Strings/51.cpp
@@ -3,32 +3,37 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
-int solve()
+bool solve()
 {
     string s1,s2;
     cin >> s1 >> s2;
 
     //logic 1
-    int l1 = s1.length(), l2 = s2.length();
-    if(l1!=l2)
-        return 0;
-    int j = 0, i = 0;
-    while(i<l1)
-    {
-        if(s1[i]==s2[j])            //ABCD,CADB
-        {
-            i++;
-            j++;
-            j %= l2;
-        }
-        else
-        {
-            j++;
-            if(j>2*l2)
-                return 0;
-        }
-    }
-    return 1;
+    // int l1 = s1.length(), l2 = s2.length();
+    // if(l1!=l2)
+    //     return 0;
+    // int j = 0, i = 0;
+    // while(i<l1)
+    // {
+    //     if(s1[i]==s2[j])            //ABCD,CADB
+    //     {
+    //         i++;
+    //         j++;
+    //         j %= l2;
+    //     }
+    //     else
+    //     {
+    //         j++;
+    //         if(j>2*l2)
+    //             return 0;
+    //     }
+    // }
+    // return 1;
+
+    //logic 2
+
+    string temp = s1 + s1;
+    return (temp.find(s2) != string::npos);
 }
 
 int main() {
