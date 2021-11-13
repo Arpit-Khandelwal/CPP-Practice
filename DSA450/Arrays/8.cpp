7e1b913421ea22c794915ec59f85584bf8145505
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-13 17:33:48 +0530

    inputs, boilerplate

---
+++ DSA450/Arrays/8.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n;i++)
+        cin >> arr[i];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test=1;
+    cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
