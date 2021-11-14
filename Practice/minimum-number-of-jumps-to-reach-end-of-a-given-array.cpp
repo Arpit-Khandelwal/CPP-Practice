7f2c7e377edaddc9de9de0f0a613187ba7c31094
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-14 17:06:26 +0530

    first draft logic

--- Practice/minimum-number-of-jumps-to-reach-end-of-a-given-array.cpp
+++ Practice/minimum-number-of-jumps-to-reach-end-of-a-given-array.cpp
@@ -8,20 +8,36 @@ void solve()
     int n;
     cin >> n;
     int arr[n];
-    for (int i = 0; i < n;i++)
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
-    
-    
+
+    int maxPos = 0, count = 0;
+    for (int i = 0; arr[i]+i < n;)
+    {
+        int max = 0;
+        for (int j = 0; j < arr[i]; j++)
+        {
+            if (arr[j] > max)
+            {
+                max = arr[j];
+                maxPos = j;
+            }
+        }
+        i = maxPos;
+        count++;
+    }
+    cout << count;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    ll test=1;
-    cin>>test;
-    while(test--)
+    ll test = 1;
+    cin >> test;
+    while (test--)
     {
         solve();
     }
