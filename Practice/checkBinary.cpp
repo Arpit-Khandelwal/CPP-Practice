6632b28ae59ac741257ca34e3e6c0537f27d688c
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-14 12:32:27 +0530

    first logic

---
+++ Practice/checkBinary.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+void solve()
+{
+    string str;
+    cin>>str;
+    int i=0;
+    //Code here
+    for(char s=str[i];i<strlen(str);i++)
+    {
+        if((int)s !=49 || (int)s!=48) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test=1;
+    cin>>test;
+    while(test--)
+    {
+        cout<< solve()<<" ";
+    }
+    return 0;
+}
