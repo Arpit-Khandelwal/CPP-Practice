71cf8b95324b06528f4f43a7cc554f5fa082b0da
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-14 17:57:35 +0530

    start over

--- Practice/minimum-number-of-jumps-to-reach-end-of-a-given-array.cpp
+++ Practice/minimum-number-of-jumps-to-reach-end-of-a-given-array.cpp
@@ -8,23 +8,25 @@ int solve()
     int a[] = {0};
     int n = sizeof(a) / sizeof(int);
 
-    if (n <= 1)
-        return 0;
+    // if (n <= 1)
+    //     return 0;
  
-    if (a[0] == 0)
-        return -1;
+    // if (a[0] == 0)
+    //     return -1;
 
-    // for (int i = 0; i < n; i++)
-    //     cin >> arr[i];
-    vector<int> arr(a, a + n);
-    int maxPos = 0, count = 0;
-    for (int i = 0; arr[i]+i < n-1;)
-    {
-        int max = max_element(arr.begin(), arr.begin() + arr[i]+1)-arr.begin();
-        i = distance(arr.begin(),max + arr.begin());
-        count++;
-    }
-    return ++count;
+    // // for (int i = 0; i < n; i++)
+    // //     cin >> arr[i];
+    // vector<int> arr(a, a + n);
+    // int maxPos = 0, count = 0;
+    // for (int i = 0; arr[i]+i < n-1;)
+    // {
+    //     int max = max_element(arr.begin(), arr.begin() + arr[i]+1)-arr.begin();
+    //     i = distance(arr.begin(),max + arr.begin());
+    //     count++;
+    // }
+    // return ++count;
+
+    
 }
 
 int main()
