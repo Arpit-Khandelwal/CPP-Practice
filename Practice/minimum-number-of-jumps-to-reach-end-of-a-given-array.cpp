1b919af238ded6bae7cbe95990813061bd293b28
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-15 19:53:00 +0530

    added gfg logic

--- Practice/minimum-number-of-jumps-to-reach-end-of-a-given-array.cpp
+++ Practice/minimum-number-of-jumps-to-reach-end-of-a-given-array.cpp
@@ -5,28 +5,49 @@ using namespace std;
 
 int solve()
 {
-    int a[] = {0};
-    int n = sizeof(a) / sizeof(int);
+    int arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+    int n = sizeof(arr) / sizeof(int);
 
-    // if (n <= 1)
-    //     return 0;
+    /*
+    if (n <= 1)
+        return 0;
  
-    // if (a[0] == 0)
-    //     return -1;
+    if (a[0] == 0)
+        return -1;
 
-    // // for (int i = 0; i < n; i++)
-    // //     cin >> arr[i];
-    // vector<int> arr(a, a + n);
-    // int maxPos = 0, count = 0;
-    // for (int i = 0; arr[i]+i < n-1;)
-    // {
-    //     int max = max_element(arr.begin(), arr.begin() + arr[i]+1)-arr.begin();
-    //     i = distance(arr.begin(),max + arr.begin());
-    //     count++;
-    // }
-    // return ++count;
+    // for (int i = 0; i < n; i++)
+    //     cin >> arr[i];
+    vector<int> arr(a, a + n);
+    int maxPos = 0, count = 0;
+    for (int i = 0; arr[i]+i < n-1;)
+    {
+        int max = max_element(arr.begin(), arr.begin() + arr[i]+1)-arr.begin();
+        i = distance(arr.begin(),max + arr.begin());
+        count++;
+    }
+    return ++count;
+    */
+    int moves = arr[0], jumps = 1;
+    int maxBound =arr[0];
+    for (int i = 1;i<n;i++) 
+    {
+        if(i==n-1)
+            return jumps;
 
-    
+        maxBound = max(maxBound, i + arr[i]);
+
+        moves--;
+
+        if(moves==0)
+        {
+            jumps++;
+
+            if(i>=maxBound)
+                return -1;
+            moves = maxBound - i;
+        }
+    }
+    return -1;
 }
 
 int main()
