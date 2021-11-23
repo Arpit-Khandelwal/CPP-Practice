aa5066704c1da6c437a24c74c44a7e0f8316238f
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-23 17:59:22 +0530

    define problem

---
+++ Codehelp/complementOfBase10.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+void solve()
+{
+    int n;
+    cin>>n; 
+
+    //flip all 1 to 0 and 0 to 1
+    //eg. input=5, output = ~(101)=010=2
+
+    
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test=1;
+    cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
