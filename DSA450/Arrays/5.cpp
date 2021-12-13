3a56326a7db5b761fd090c2c50c47ed4e64821eb
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-12-13 23:17:44 +0530

    tests done

--- DSA450/Arrays/5.cpp
+++ DSA450/Arrays/5.cpp
@@ -12,7 +12,7 @@ void solve()
     // for (int i = 0; i < n; i++)
     //     cin >> arr[i];
 
-    int arr[] = {-12, 11, -13, -5,6, -7, 5, -3, 11};
+    int arr[] = {-12, 11, -13, -5,6, -7, 5, -3, -6};
      int n = sizeof(arr) /sizeof(arr[0]);
 
     int l = 0, h = n - 1;
