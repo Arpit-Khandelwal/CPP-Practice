bd5d99ff57d88e5f5a4a39fb846166dce2259747
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-01-10 17:37:26 +0530

    explanation of logic 1

--- DSA450/Bit Manipulation/Non Repeating Numbers.cpp
+++ DSA450/Bit Manipulation/Non Repeating Numbers.cpp
@@ -8,6 +8,15 @@ vector<int> singleNumber(vector<int> nums)
         xorred ^= nums[i];
     }
     int set_bit = xorred & ~(xorred - 1);
+    //set_bit = the bit where both numbers are diff
+    /*
+        eg. 1,2,3,4,2,1, xor = 7(111)
+        set_bit = 111 & ~(111-1) = 111 & ~(110) = 111 & 011 = 011, these 3 bits are diff in distinct numbers
+        1 xor 7 = 110;6 - grp2
+        2 xor 7 = 101;5 - grp1
+        3 xor 7 = 100;4 - grp2
+        4 xor 7 = 011;3 - grp1
+    */
 
     vector<int> grp1;
     vector<int> grp2;
@@ -15,9 +24,9 @@ vector<int> singleNumber(vector<int> nums)
     for (int i = 0; i < nums.size(); i++)
     {
         if (nums[i] & set_bit)
-            dist1 ^= nums[i];
+            dist1 ^= nums[i]; //xorring all grp1 elements, duplicates cancel out, only distinct number left
         else
-            dist2 ^= nums[i];
+            dist2 ^= nums[i]; //xorring all grp2 elements, duplicates cancel out, only distinct number left
     }
 
     ans.push_back(dist1);
