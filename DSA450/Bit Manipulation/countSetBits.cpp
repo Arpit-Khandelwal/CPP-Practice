b9513884fd77938defe6c449d8cdd571f703cbbc
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-01-10 16:46:34 +0530

    added logic 2

--- DSA450/Bit Manipulation/countSetBits.cpp
+++ DSA450/Bit Manipulation/countSetBits.cpp
@@ -11,5 +11,13 @@ int setBits(int N)
         n = n >> 1;
     }
 
+    /*logic-2
+
+        for(int i=0;i<sizeof(int)*8;i++)
+        {
+            if(N&(1<<i)) count++;
+        }
+    */
+
     return count;
 }
