387b260f82a797f5924917e3b64153e318435eca
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-01-13 00:24:11 +0530

    example problem

---
+++ Practice/out.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+void solve()
+{
+    ll int x;
+}
+
+int main()
+{
+    cout << (ll int) pow(2, 29);
+    solve();
+
+    return 0;
+}
