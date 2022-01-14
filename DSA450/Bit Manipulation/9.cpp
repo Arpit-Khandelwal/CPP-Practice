0e575b91d24a861bfa57ea3a35b551bc85c828e6
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-01-15 02:14:41 +0530

    doneee!

---
+++ DSA450/Bit Manipulation/9.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int ans = 0, i = 0;
+    for (int k = n; k>0;k>>=1,i++)
+    {
+        if (k & 1)
+        {
+            ans += n << i;
+        }
+        
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test = 1;
+    cin >> test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
+}
