5675b0b92d2d30d304de25619e2b2ba39f7c6dcc
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-01-21 00:01:22 +0530

    add more ways of solving

--- DSA450/Arrays/4.cpp
+++ DSA450/Arrays/4.cpp
@@ -7,18 +7,18 @@ void solve()
 {
     int n;
     cin >> n;
-    int arr[n];
+    int a[n];
 
     for (int i = 0; i < n; i++)
-        cin >> arr[i];
+        cin >> a[i];
 
     // int ans[n];
     // int l = 0, h = n - 1;
     // for (int i = 0; i < n; i++)
     // {
-    //     if (arr[i] == 0)
+    //     if (a[i] == 0)
     //         ans[l++] = 0;
-    //     if (arr[i] == 2)
+    //     if (a[i] == 2)
     //         ans[h--] = 2;
     // }
     // while (l <= h)
@@ -26,27 +26,70 @@ void solve()
 
     // for (int i = 0; i < n; i++)
     //     cout << ans[i] << " ";
-
+    /*
     int p0 = 0, p1 = 0, p2 = 0;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] == 0)
+        if (a[i] == 0)
             p0++;
-        else if (arr[i] == 1)
+        else if (a[i] == 1)
             p1++;
-        else if (arr[i] == 2)
+        else if (a[i] == 2)
             p2++;
     }
     int i = 0;
     for (; i < p0; i++)
-        arr[i] = 0;
+        a[i] = 0;
     for (; i < p0 + p1; i++)
-        arr[i] = 1;
+        a[i] = 1;
     for (; i < p0 + p1 + p2; i++)
-        arr[i] = 2;
+        a[i] = 2;
+*/
+
+    /*count 0s,1s,2s and put in array
+    int count0=0, count1=0, count2=0;
 
     for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    {
+        switch (a[i])
+        {
+        case 0:
+            count0++;
+            break;
+        case 1:
+            count1++;
+            break;
+        default:
+            count2++;
+        }
+    }
+    int i = 0;
+    while (count0-- > 0) a[i++] = 0;
+    while (count1-- > 0) a[i++] = 1;
+    while (count2-- > 0) a[i++] = 2;
+    */
+   
+   //check and swap - O(N)  complexity
+   //Method 1 of https://www.geeksforgeeks.org/sort-an-array-of-0s-1s-and-2s/ 
+    int low = 0, mid = 0, high = n - 1;
+
+    while (mid <= high)
+    {
+        switch (a[mid])
+        {
+        case 0:
+            swap(a[low++], a[mid++]);
+            break;
+        case 1:
+            mid++;
+            break;
+        case 2:
+            swap(a[mid], a[high--]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
 }
 
 int main()
