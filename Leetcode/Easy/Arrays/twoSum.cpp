b154ff54971f500f712ce5da0a1b2e7e089a4fda
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-01-20 15:57:09 +0530

    add to array folder

--- Leetcode/Easy/Arrays/twoSum.cpp
+++ Leetcode/Easy/Arrays/twoSum.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<int> twoSum(vector<int> &nums, int target)
 {
-    //O(N^2) - bruteforce
+    /*O(N^2) - bruteforce
     for (int i = 0; i < nums.size(); i++)
     {
         for (int j = i + 1; j < nums.size(); j++)
@@ -13,6 +13,18 @@ vector<int> twoSum(vector<int> &nums, int target)
                 return vector<int>{i, j};
     }
     return vector<int>{-1};
+    */
+
+    //hashmap
+    map<int, int> hashmap;
+    for (int i = 0; i < nums.size(); i++)
+        hashmap.insert(nums[i], i);
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (hashmap.find(target - nums[i]) != hashmap.end())
+            return vector<int>{i, hashmap.find(target - nums[i])->second};
+    }
+    return vector<int>{-1}; 
 }
 
 void solve()
