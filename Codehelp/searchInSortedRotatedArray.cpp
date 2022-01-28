e9cba6560a04dd53a9dd0ad52af9b9a410ed0483
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-01-29 01:38:56 +0530

    add new logic

--- Codehelp/searchInSortedRotatedArray.cpp
+++ Codehelp/searchInSortedRotatedArray.cpp
@@ -5,13 +5,14 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
+/* babbar bhaiya's approach
 int binarySearch(vector<int> arr, int l, int h, int k)
 {
     int mid;
     while (l <= h)
     {
         mid = (l + h) / 2; //works for almost all cases except when l+h > INT_MAX
-        //therefore, we can compute mid as
+        //therefore, we can compute mid as the following
         mid = l + ((h - l) / 2); //handles Overflow of INT_MAX
         if (k > arr[mid])
             l = mid + 1;
@@ -46,6 +47,48 @@ int findPosition(vector<int> &arr, int n, int k)
     else
         return binarySearch(arr, 0, pivot - 1, k);
 }
+*/
+
+//my approach
+    int binarySearch(vector<int>& arr, int l, int h, int k)
+    {
+        int mid;
+        while(l<=h)
+        {
+            mid = l + (h-l)/2;
+            if(arr[mid]<k) l=mid+1;
+            else if(arr[mid]>k) h=mid-1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    int findPosition(vector<int>& arr, int n, int k)
+    {
+        // Write your code here.
+        // Return the position of K in ARR else return -1.
+        int l=0,h=arr.size()-1, mid = l + (h-l)/2;;
+        if(arr[0]<arr[n-1]) return binarySearch(arr,l,h,k);
+        
+        while(l<=h)
+        {
+        if(k>arr[mid])
+        {
+            if(k>arr[l]) l=mid+1;
+            else h=mid-1;
+        }
+        else if(k<arr[mid])
+        {
+            if(k<arr[l]) l = mid+1; 
+            else h=mid-1;
+        }
+        else return l;
+            mid = l + (h-l)/2;
+        }
+        return -1;
+        
+    }
+
 
 void solve()
 {
