c418f5cf784712901338faf8b71acc9fa9751a46
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-01-30 12:59:36 +0530

    beautify

--- Codehelp/paintersPartition.cpp
+++ Codehelp/paintersPartition.cpp
@@ -1,18 +1,24 @@
-#include<bits/stdc++.h>
-#include<iostream>
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 bool isPossible(vector<int> &boards, int k, int mid, int n)
 {
     int painters = 1;
     int sum = 0;
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        if(sum + boards[i] <= mid) sum+=boards[i];
+        if (sum + boards[i] <= mid)
+        {
+            sum += boards[i];
+        }
         else
         {
             painters++;
-            if(painters > k || boards[i] > mid) return false;
+            if (painters > k || boards[i] > mid)
+            {
+                return false;
+            }
             sum = boards[i];
         }
     }
@@ -22,24 +28,28 @@ bool isPossible(vector<int> &boards, int k, int mid, int n)
 int findLargestMinDistance(vector<int> &boards, int k)
 {
     //    Write your code here.
-    int l,h,mid;
+    int l, h, mid;
     int n = boards.size();
     int sum = 0;
-    for(int i=0;i<n;i++) sum+=boards[i];
-    l=0;
-    h=sum;
-    mid = l + (h - l)/2;
-    int ans = -1;
-    while(l<=h)
+    for (int i = 0; i < n; i++)
     {
-        if(isPossible(boards, k, mid, n))
+        sum += boards[i];
+    }
+    l = 0;
+    h = sum;
+    mid = l + (h - l) / 2;
+    int ans = -1;
+    while (l <= h)
+    {
+        if (isPossible(boards, k, mid, n))
         {
             ans = mid;
             h = mid - 1;
         }
-        else l = mid + 1;
-        
-        mid = l + (h - l)/2;
+        else
+            l = mid + 1;
+
+        mid = l + (h - l) / 2;
     }
     return ans;
 }
@@ -50,7 +60,7 @@ int main()
     cin >> n >> k;
     vector<int> boards;
     int x;
-    for (int i = 0; i < n;i++) 
+    for (int i = 0; i < n; i++)
     {
         cin >> x;
         boards.push_back(x);
