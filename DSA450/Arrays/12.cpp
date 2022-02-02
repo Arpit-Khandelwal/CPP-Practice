787e79216a3f245bf9478d2b6d42368cbe5013fb
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-02-02 11:21:25 +0530

    first draft of gap logic and docs:explain previous logic

--- DSA450/Arrays/12.cpp
+++ DSA450/Arrays/12.cpp
@@ -3,19 +3,45 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
-void mergeArray(int arr1[],int n, int arr2[],int m)
+void mergeArray(int arr1[], int n, int arr2[], int m)
 {
+    //bruteforce
+    /*
     for (int i = 0; i < n;i++)
     {
         if(arr1[i]>arr2[0])
         {
             swap(arr1[i], arr2[0]);
-
+            //sort(arr2,arr2+m) can be done instead of following lines, but takes more time
             int k = 1, temp = arr2[0];
             while(arr2[k]<temp && k<m)
-                arr2[k - 1] = arr2[k++];
+                arr2[k - 1] = arr2[k++];    //push elements forward to put arr2[0],which was originally arr1[i], in the sorted position in arr2
             arr2[k - 1] = temp;
         }
+    }
+    */
+
+    //gap method - compare elements with gap and swap if arr1[i]<arr1/arr2[i+gap]
+    int gap = ceil(n / 2);
+    while (gap > 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int secondIndex = gap + i;
+            if (secondIndex < n)
+            {
+                if (arr1[i] > arr1[secondIndex])
+                    swap(arr1[i], arr1[secondIndex]);
+            }
+            else if (secondIndex < n + m)
+            {
+                if (arr1[i] > arr2[secondIndex - n])
+                    swap(arr1[i], arr2[secondIndex - n]);
+            }
+            else
+                break;
+        }
+        gap /= 2;
     }
 }
 
@@ -25,16 +51,16 @@ void solve()
     cin >> n >> m;
     int arr1[n], arr2[m];
 
-    for (int i = 0; i < n;i++)
+    for (int i = 0; i < n; i++)
         cin >> arr1[i];
-    for (int j = 0; j < m;j++)
+    for (int j = 0; j < m; j++)
         cin >> arr2[j];
 
     mergeArray(arr1, n, arr2, m);
-    for (int i = 0; i < n;i++)
-        cout<< arr1[i]<<" ";
-    for (int j = 0; j < m;j++)
-        cout<< arr2[j]<<" ";
+    for (int i = 0; i < n; i++)
+        cout << arr1[i] << " ";
+    for (int j = 0; j < m; j++)
+        cout << arr2[j] << " ";
 
     cout << endl;
 }
