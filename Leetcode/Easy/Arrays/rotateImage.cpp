b8b45cbe0f4b622dd65b2dc1dcc295755cb07001
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-02-03 16:32:33 +0530

    add method 2

--- Leetcode/Easy/Arrays/rotateImage.cpp
+++ Leetcode/Easy/Arrays/rotateImage.cpp
@@ -13,3 +13,23 @@ void rotate(vector<vector<int>> &matrix)
         }
     }
 }
+
+//method 2:
+void rotate(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n / 2; j++)
+        {
+            swap(matrix[i][j], matrix[i][n - j - 1]);
+        }
+    }
+}
