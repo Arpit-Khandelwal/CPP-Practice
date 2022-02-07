ceadb417edb34258759597bab0594a4270f517d4
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-02-07 12:45:20 +0530

    add  fixed array approach, needs debugging

--- Leetcode/Easy/Strings/FirstUniqueCharacter.cpp
+++ Leetcode/Easy/Strings/FirstUniqueCharacter.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int firstUniqChar(string s)
 {
+    //using hashmap to count all characters number of occourences
+    /*
     unordered_map<char,int> map;
     int l = s.length();
 
@@ -17,6 +19,23 @@ int firstUniqChar(string s)
     for (int i = 0; i < l;i++)
     {
         if(map[s[i]]==1)
+            return i;
+    }
+    return -1;
+    */
+
+    //using fixed char[27]
+    int l = s.length();
+    char arr[27];
+    for (int i = 0; i < 27; i++) arr[i] = 0;
+
+
+    for (int i = 0; i < l; i++) arr[s[i] - 'a']++;
+
+    for (int i = 0; i < l;i++)
+
+    {
+        if(arr[s[i] - 'a'] == 1)
             return i;
     }
     return -1;
--- Leetcode/Easy/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-unordered_map<int, int> hashmap;
-    for (int i = 0; i < nums.size(); i++)
-        hashmap.insert(nums[i], i);
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (hashmap.find(target - nums[i]) != hashmap.end())
-            return vector<int>{i, hashmap.find(target - nums[i])->second};
-    }
-    return vector<int>{-1}; 
