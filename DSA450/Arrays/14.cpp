c393a7d4e24367b8a01ad23bc823fb583eb840c0
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-02-09 18:40:24 +0530

    draft

--- DSA450/Arrays/14.cpp
+++ DSA450/Arrays/14.cpp
@@ -3,14 +3,50 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
+
+void printVector(vector<vector<int>> &ans)
+{
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i][0] << " " << ans[i][1] << endl;
+    }
+}
+
 vector<vector<int>> merge(vector<vector<int>> &intervals)
 {
-    
+    sort(intervals.begin(), intervals.end());
+
+    printVector(intervals);
+
+    int n = intervals.size();
+    vector<vector<int>> ans;
+
+    // ans.push_back({
+    //                 min(intervals[0][0], intervals[1][0]),
+    //                 max(intervals[0][1], intervals[1][1])
+    //                 });
+
+    for (int i = 0,j=1; j < n - 1;j++)
+    {
+        if(ans[i][1]>=intervals[j][0])
+        {
+            ans[i][0] = min(ans[i][0], intervals[j][0]);
+            ans[i][1] = max(ans[i][1], intervals[j][1]);
+        }
+        else
+        {
+            ans.push_back(intervals[j]);
+            i++;
+        }
+    }
+
+        return ans;
 }
 
 void solve()
 {
     int n;
+    cin >> n;
     vector<vector<int>> intervals;
     for (int i = 0; i < n; i++)
     {
@@ -21,10 +57,7 @@ void solve()
 
     vector<vector<int>> ans = merge(intervals);
 
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i][0] << " " << ans[i][1] << endl;
-    }
+    printVector(ans);
 }
 
 int main()
