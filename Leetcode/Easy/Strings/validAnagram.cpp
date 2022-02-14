d97d87be8e8dbd1c904e8df290359bb9df8a6a51
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-02-14 22:10:35 +0530

    add length condition

--- Leetcode/Easy/Strings/validAnagram.cpp
+++ Leetcode/Easy/Strings/validAnagram.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 bool isAnagram(string s, string t)
 {
+    if(s.length()!=t.length())
+        return false;
     //sort s and t and check if they are same
     sort(s.begin(), s.end());
     sort(t.begin(), t.end());
