073279a5cd3aa81ed0019d38b8af3ff8b5d24be9
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-02-14 22:04:56 +0530

    boilerplate

---
+++ Leetcode/Easy/Strings/validAnagram.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+bool isAnagram(string s, string t)
+{
+    
+}
+
+void solve()
+{
+    string s, t;
+    cin >> s >> t;
+
+    cout << isAnagram(s, t) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test = 1;
+    cin >> test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
+}
