40071694d0c643effb44f5deb9814e09ead0c099
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-02-23 12:20:17 +0530

    in progress

--- codechef/MULTHREE.cpp
+++ codechef/MULTHREE.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
-void solve()
+string solve()
 {
     ll int k;
     int d0, d1;
@@ -11,28 +11,41 @@ void solve()
     cin >> k >> d0 >> d1;
 
     ll int sum = d0 + d1;
-    k -= 2;
+    /* bruteforce
     ll int nextD;
+    k -= 2;
 
     while(k--)
     {
         nextD = sum%10;
         sum += nextD;
     }
+    */
 
-    cout << sum << " " << ((sum % 3 == 0) ? "YES" : "NO")<<endl;
+    if(k==2)
+        return ((sum % 3 == 0) ? "YES" : "NO");
+    else if(k==3)
+    {
+        sum *= 2;
+        return ((sum % 3 == 0) ? "YES" : "NO");
+    }
+    else
+    {
+       
+    }
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    ll test=1;
-    cin>>test;
-    while(test--)
+    ll test = 1;
+    cin >> test;
+    while (test--)
     {
-        solve();
+        cout << solve() << endl;
     }
     return 0;
 }
