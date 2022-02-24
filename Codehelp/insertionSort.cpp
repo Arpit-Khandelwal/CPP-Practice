ddf718692f4d6508b5b21b3500714237622c5102
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-02-24 16:40:53 +0530

    full code done

--- Codehelp/insertionSort.cpp
+++ Codehelp/insertionSort.cpp
@@ -1,3 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
 void insertionSort(int n, vector<int> &arr)
 {
     // Write your code here.
@@ -6,7 +11,11 @@ void insertionSort(int n, vector<int> &arr)
     {
         int temp = arr[i];
         j = i - 1;
-        while (j >= 0 && arr[j] > temp) arr[j + 1] = arr[j--];
+        while (j >= 0 && arr[j] > temp) 
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
         // for loop variation
         /*
             for(;j>=0;j--)
@@ -17,4 +26,40 @@ void insertionSort(int n, vector<int> &arr)
         */
         arr[j + 1] = temp;
     }
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    insertionSort(n, arr);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test = 1;
+    cin >> test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
 }
