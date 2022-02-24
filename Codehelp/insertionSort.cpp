2cde1a14c4afb34f0f186851ee137be7f1ea9fef
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-02-24 16:33:38 +0530

    add for loop variation

--- Codehelp/insertionSort.cpp
+++ Codehelp/insertionSort.cpp
@@ -7,6 +7,14 @@ void insertionSort(int n, vector<int> &arr)
         int temp = arr[i];
         j = i - 1;
         while (j >= 0 && arr[j] > temp) arr[j + 1] = arr[j--];
+        // for loop variation
+        /*
+            for(;j>=0;j--)
+            {
+                if(arr[j]>temp) arr[j+1]=arr[j];
+                else break;
+            }
+        */
         arr[j + 1] = temp;
     }
 }
