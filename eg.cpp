5624b3aaf9be222da340e3fd4b85d61827abf382
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-03-20 11:30:19 +0530

    JP morgan

--- eg.cpp
+++ eg.cpp
@@ -5,15 +5,52 @@ using namespace std;
 
 void solve()
 {
-    
+    multiset<int> num;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        num.insert(x);
+    }
+
+    sort(num.begin(), num.end());
+
+    int cost = 0;
+    int index = 0;
+    int sum = 0;
+
+    while (num.lower_bound(1) != num.end())
+    {
+        multiset<int>::iterator itr = num.lower_bound(1);
+
+        sum = *(num.lower_bound(1)) + *(++itr);
+
+        num.erase(num.lower_bound(1));
+
+
+        index += 2;
+        cost += sum;
+
+        num.insert(sum);
+    }
+
+    cout << cost;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cout << 1<<100<<endl; // 1100
-    cout << (1 << 100); //overflow
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
     return 0;
 }
