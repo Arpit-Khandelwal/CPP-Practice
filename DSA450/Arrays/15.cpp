8025be622d7bc332ffe9d33c391483557a4de01c
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-03-24 19:05:16 +0530

    logic1

--- DSA450/Arrays/15.cpp
+++ DSA450/Arrays/15.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 void nextPermutation(vector<int> &nums)
 {
-    
+    int n = nums.size();
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        if (nums[i - 1] < nums[i])
+        {
+            swap(nums[i - 1], nums[i]);
+            return;
+        }
+    }
+    sort(nums.begin(), nums.end());
+    return;
 }
 
 void solve()
