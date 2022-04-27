db14198be34ff63942e3fc7cbec3c9a532eb2426
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-04-27 21:21:51 +0530

    done

--- Codehelp/maxOccouringCharacter.cpp
+++ Codehelp/maxOccouringCharacter.cpp
@@ -1,3 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
 char getMaxOccuringChar(string str)
 {
     unordered_map<char, int> count;
@@ -18,4 +23,27 @@ char getMaxOccuringChar(string str)
             ans = min(i.first, ans);
     }
     return ans;
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    cout << getMaxOccuringChar(s) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test = 1;
+    cin >> test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
 }
