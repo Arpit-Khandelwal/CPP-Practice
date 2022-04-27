b84273b710c3aa3ce6dc93ae9e711bbf6bdd3df2
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-04-27 20:01:07 +0530

    add 2 pointer

--- Leetcode/Easy/Strings/reverseString.cpp
+++ Leetcode/Easy/Strings/reverseString.cpp
@@ -6,4 +6,12 @@ void reverseString(vector<char> &s)
     int l = s.size();
     for (int i = 0; i < l / 2; i++)
         swap(s[i], s[l - i - 1]);
+
+    /*
+    //using 2 pointers
+    int l=0,r=s.size()-1;
+    while(l<r) swap(s[l++],s[r--]);
+    */
+
+
 }
