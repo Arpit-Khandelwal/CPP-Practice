17ded2db879eb391efd61f7bc4bc4f683c019046
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-05-24 02:39:31 +0530

    done

--- Leetcode/Easy/Strings/RemoveAllAdjacentDuplicatesInString.cpp
+++ Leetcode/Easy/Strings/RemoveAllAdjacentDuplicatesInString.cpp
@@ -1,3 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
 /*
 int findAdjacentDuplicate(string s)
 {
@@ -35,4 +40,26 @@ string removeDuplicates(string s)
         
         return ans;
 }
+void solve()
+{
+    string s;
+    cin >> s;
+    cout << removeDuplicates(s) << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test=1;
+    cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
 
