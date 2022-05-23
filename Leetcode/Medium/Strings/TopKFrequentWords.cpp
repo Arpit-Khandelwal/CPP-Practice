899797d9fdcfd217f4a3530be12293355be4bfed
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-05-24 02:00:02 +0530

    boilerplate

--- Leetcode/Medium/Strings/TopKFrequentWords.cpp
+++ Leetcode/Medium/Strings/TopKFrequentWords.cpp
@@ -1,3 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
 static bool cmp(pair<string, int> a, pair<string, int> b)
 {
     if (a.second > b.second)
@@ -30,4 +35,23 @@ vector<string> topKFrequent(vector<string> &words, int k)
         ans.push_back(vFreq[i].first);
 
     return ans;
+}
+
+void solve()
+{
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test=1;
+    cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
 }
