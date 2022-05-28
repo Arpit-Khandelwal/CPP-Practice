72f9c1304d6ca24dd790745a1ac7ffec731ca315
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-05-28 18:43:06 +0530

    better method

--- Codehelp/Lecture 23/RotateImage.cpp
+++ Codehelp/Lecture 23/RotateImage.cpp
@@ -5,6 +5,16 @@ void rotate(vector<vector<int>> &matrix)
 {
     int n = matrix.size();
 
+    // simpler method - my first intuition
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+
+    for (int i = 0; i < n; i++) reverse(matrix[i].begin(), matrix[i].end());
+
+    // my method
     for (int i = 0; i < n / 2; i++)
     {
         for (int count = 0; count < n - 2 * i - 1; count++)
