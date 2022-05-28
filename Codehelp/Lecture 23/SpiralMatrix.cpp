dd5b46f1400a810bd707a1faa8b96957cd9b14be
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-05-28 13:22:38 +0530

    done

--- Codehelp/Lecture 23/SpiralMatrix.cpp
+++ Codehelp/Lecture 23/SpiralMatrix.cpp
@@ -10,20 +10,28 @@ vector<int> spiralOrder(vector<vector<int>> &matrix)
 
     vector<int> ans;
     int startRow = 0, startCol = 0, endRow = r-1, endCol = c-1;
-    while (count <= r * c)
+    while (count < r * c)
     {
-        for (int i = startCol; i <= endCol && count<=r*c;i++,count++) ans.push_back(matrix[startRow][i]);
+        for (int i = startCol; i <= endCol && count<r*c;i++,count++) ans.push_back(matrix[startRow][i]);
         startRow++;
         
-        for (int i = startRow;i<=endRow &&  count <= r * c;i++,count++) ans.push_back(matrix[i][endCol]);
+        for (int i = startRow;i<=endRow &&  count < r * c;i++,count++) ans.push_back(matrix[i][endCol]);
         endCol--;
 
-        for (int i = endCol;i>=startCol && count <= r * c;i--,count++) ans.push_back(matrix[endRow][i]);
+        for (int i = endCol;i>=startCol && count < r * c;i--,count++) ans.push_back(matrix[endRow][i]);
         endRow--;
 
-        for (int i = endRow;i>=startRow && count <= r * c;i--,count++) ans.push_back(matrix[i][startCol]);
+        for (int i = endRow;i>=startRow && count < r * c;i--,count++) ans.push_back(matrix[i][startCol]);
         startCol++;
     }
 
     return ans;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = { {1,2,3},{4,5,6},{7,8,9} };
+    vector<int> ans = spiralOrder(matrix);
+    for (int i = 0; i < ans.size(); i++)
+        cout << ans[i] << " ";
 }
