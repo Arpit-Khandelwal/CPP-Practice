e90c762de0c8542ea8a6465d1057c920b2901c63
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-06-07 21:03:04 +0530

    add map method

--- Leetcode/Easy/Arrays/containsDuplicate.cpp
+++ Leetcode/Easy/Arrays/containsDuplicate.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 bool containsDuplicate(vector<int> &nums)
 {
 
     /*
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++) 
+        for(int i=0;i<nums.size()-1;i++)
             if(!(nums[i]^nums[i+1])) return true;
         return false;
         */
@@ -18,6 +17,16 @@ bool containsDuplicate(vector<int> &nums)
         if (arr.find(num) != arr.end())
             return true;
         arr.insert(num);
+    }
+    return false;
+
+    // method 2
+    unordered_map<int, bool> map;
+
+    for (int i : nums)
+    {
+        if (map[i]) return true;
+        map[i] = true;
     }
     return false;
 }
