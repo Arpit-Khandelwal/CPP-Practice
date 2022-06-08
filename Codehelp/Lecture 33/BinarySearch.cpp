b6cfc6b205604da978e0587f678a46ee24ecbf59
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-06-08 16:48:26 +0530

    bugs squashed

--- Codehelp/Lecture 33/BinarySearch.cpp
+++ Codehelp/Lecture 33/BinarySearch.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int recursiveBinarySearch(int l, int h, int *arr, int n)
+int recursiveBinarySearch(int l, int h, int *arr, int val)
 {
     if (l > h)
         return -1;
     int mid = l + (h - l) / 2;
-    if (arr[mid] == mid)
+    if (arr[mid] == val)
         return mid;
-    else if (arr[mid] > mid)
-        return recursiveBinarySearch(l, mid - 1, arr, n);
+    else if (arr[mid] > val)
+        return recursiveBinarySearch(l, mid - 1, arr, val);
     else
-        return recursiveBinarySearch(mid + 1, h, arr, n);
+        return recursiveBinarySearch(mid + 1, h, arr, val);
 }
 
 int binarySearch(int *input, int n, int val)
 {
 
     //Write your code here
-    return recursiveBinarySearch(0, n - 1, input, n);
+    return recursiveBinarySearch(0, n - 1, input, val);
 }
 
 int main()
