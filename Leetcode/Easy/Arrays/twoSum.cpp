d0628f3b71047c7de6a6e4955728a15e6d0cc7f8
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-06-16 13:07:42 +0530

    add user input

--- Leetcode/Easy/Arrays/twoSum.cpp
+++ Leetcode/Easy/Arrays/twoSum.cpp
@@ -29,6 +29,10 @@ vector<int> twoSum(vector<int> &nums, int target)
 
 void solve()
 {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    cout << result[0] << " " << result[1] << endl;
 }
 
 int main()
