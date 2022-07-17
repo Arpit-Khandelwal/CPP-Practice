5bb0969efb2eb06e2ad1afcc703fae8a60f67ed8
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-17 18:47:07 +0530

    boilerplate

--- Leetcode/Easy/Others/missingNumber.cpp
+++ Leetcode/Easy/Others/missingNumber.cpp
@@ -1,3 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+
 int missingNumber(vector<int> &nums)
 {
 
@@ -9,3 +15,35 @@ int missingNumber(vector<int> &nums)
 
     return sum;
 }
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> nums;
+    for (int i = 0; i < n;i++)
+    {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+
+    cout << missingNumber(nums)<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test=1;
+    cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
