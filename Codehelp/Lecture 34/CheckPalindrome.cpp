b3b2096c0529689c4cada294b3d3c5d33c5a0c8e
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-18 16:04:00 +0530

    done

--- Codehelp/Lecture 34/CheckPalindrome.cpp
+++ Codehelp/Lecture 34/CheckPalindrome.cpp
@@ -3,9 +3,24 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
+bool checkPalindrome(string s, int i)
+{
+    int n = s.length();
+    if(i==n-1)
+        return true;
+    
+    if(s[i]==s[n-i-1])
+        return checkPalindrome(s, i + 1);
+    else
+        return false;
+}
+
 void solve()
 {
-    
+    string s;
+    cin >> s;
+
+    cout<<checkPalindrome(s,0)<<endl;
 }
 
 int main() {
