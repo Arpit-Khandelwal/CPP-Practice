4e79008e51c5f10a36aa56d350a95d21b284c85e
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-20 10:32:04 +0530

    done

--- Codehelp/Lecture 34/insertionSortRecursion.cpp
+++ Codehelp/Lecture 34/insertionSortRecursion.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 void insertionSortRecursive(int* arr, int i, int n)
 {
-    if(i==n-1)
+    if(i==n)
         return;
 
     int temp = arr[i];
     int j = i - 1;
     while (j >= 0 && arr[j]>temp)
-        arr[j + 1] = arr[j--];
-    for (int i = 0; i < n;i++)  cout << arr[i] << " ";
+    {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+        
+    // for (int k = 0; k < n;k++)  cout << arr[k] << " ";
+    cout <<j<< endl;
     arr[j + 1] = temp;
 
     return insertionSortRecursive(arr, i + 1, n);
