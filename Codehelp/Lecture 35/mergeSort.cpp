47a934b3b39dbb9303fba73fc0a046fe6f964b3a
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-20 11:16:02 +0530

    done

--- Codehelp/Lecture 35/mergeSort.cpp
+++ Codehelp/Lecture 35/mergeSort.cpp
@@ -3,6 +3,53 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
+void merge(int *arr, int s, int e)
+{
+    int mid = s + (e - s) / 2;
+
+    int l1 = mid - s + 1;
+    int l2 = e - mid;
+
+    int arr1[l1], arr2[l2];
+
+    //copy arr[s,mid] in arr1 and arr[mid+1,e] in arr2
+    int k = s;
+    for (int i = 0; i < l1;i++)
+        arr1[i] = arr[k++];
+    k = mid+1;
+    for (int i = 0; i < l2;i++)
+        arr2[i] = arr[k++];
+
+    //merge two sorted arrays
+    k = s;
+    int i1 = 0, i2 = 0;
+    while( i1 < l1 && i2 < l2)
+    {
+        if(arr1[i1]<arr2[i2])
+            arr[k++] = arr1[i1++];
+        else
+            arr[k++] = arr2[i2++];
+    }
+
+    while(i1<l1) arr[k++]=arr1[i1++];
+    while(i2<l2) arr[k++]=arr1[i2++];
+
+    return;
+}
+
+void mergeSort(int *arr, int s, int e)
+{
+    if(s>=e)
+        return;
+
+    int mid = s + (e - s) / 2;
+
+    mergeSort(arr, s, mid);
+    mergeSort(arr, mid + 1, e);
+
+    merge(arr, s, e);
+}
+
 void solve()
 {
     int n;
@@ -13,7 +60,7 @@ void solve()
         cin >> arr[i];
 
     //mergeSort recursive
-
+    mergeSort(arr, 0, n - 1);
     for(int i:arr)
         cout << i<<" ";
     cout << endl;
