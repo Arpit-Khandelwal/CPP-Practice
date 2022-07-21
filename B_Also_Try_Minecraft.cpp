eaa5219fd50924473f662eab709a79fc2018ec7d
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-21 22:05:59 +0530

    TLE on test 2

--- B_Also_Try_Minecraft.cpp
+++ B_Also_Try_Minecraft.cpp
@@ -3,14 +3,7 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
-long long int findFallDamage(vector<int> heights, int a, int b)
-{
-    ll int damage = 0;
-
-    return damage;
-}
-
-long long int solve(vector<int> heights)
+long long int solve(vector<ll int> prefix, vector<ll int> suffix)
 {
     int a, b;
     cin >> a >> b;
@@ -18,19 +11,13 @@ long long int solve(vector<int> heights)
     ll int damage = 0;
     if (a < b)
     {
-        for (int i = a; i < b; i++)
-        {
-            if (heights[i] > heights[i + 1])
-                damage += heights[i] - heights[i+1];
-        }
+        // for (int i = a; i < b; i++) damage += prefix[i];
+        return prefix[b-1] - prefix[a - 1];
     }
     else
     {
-        for (int i = a; i > b;i--)
-        {
-            if(heights[i]>heights[i-1])
-                damage += heights[i] - heights[i - 1];
-        }
+        // for (int i = a - 1; i >= b; i--) damage += suffix[i];
+        return suffix[b+1] - suffix[a+1];
     }
 
     return damage;
@@ -45,15 +32,37 @@ int main()
     int n, m;
     cin >> n >> m;
 
-    vector<int> heights(n+1);
+    vector<int> heights(n + 1);
     for (int i = 1; i <= n; i++)
     {
         cin >> heights[i];
     }
 
+    vector<ll int> prefix, suffix(n + 1);
+    prefix.push_back(0);
+    suffix[0] = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (heights[i] > heights[i + 1])
+            prefix.push_back(heights[i] - heights[i + 1]);
+        else
+            prefix.push_back(0);
+    }
+    for (int i = 1; i <= n; i++) prefix[i] += prefix[i - 1];
+
+    for (int i = n; i > 1; i--)
+    {
+        if (heights[i] > heights[i - 1])
+            suffix[i] = heights[i] - heights[i - 1];
+        else
+            suffix[i] = 0;
+    }
+    for (int i = n - 1; i >= 1;i--) suffix[i]+=suffix[i+1];
+
     while (m--)
     {
-        cout << solve(heights) << endl;
+        cout << solve(prefix, suffix) << endl;
     }
     return 0;
 }
