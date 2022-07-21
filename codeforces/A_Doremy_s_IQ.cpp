299db668839ebf786e82c5eff0a136b2f664e71c
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-21 19:06:58 +0530

    draft

---
+++ codeforces/A_Doremy_s_IQ.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+void solve()
+{
+    int a, q;
+    cin >> a >> q;
+
+    vector<int> difficulty;
+
+    int x;
+    for (int i = 0; i < a; i++)
+    {
+        cin >> x;
+        difficulty.push_back(x);
+    }
+    // sort(difficulty.begin(), difficulty.end());
+    // input done
+    string s = "";
+    for (int i : difficulty)
+    {
+        if (q == 0)
+        {
+            s += '0';
+            break;
+        }
+        if (i > q)
+        {
+            q--;
+        }
+
+        s += '1';
+    }
+    
+    cout << s << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll test = 1;
+    cin >> test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
+}
