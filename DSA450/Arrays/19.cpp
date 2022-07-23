658e635ef22e492271552451789f55b3f815d694
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-23 18:50:53 +0530

    solve() done

--- DSA450/Arrays/19.cpp
+++ DSA450/Arrays/19.cpp
@@ -3,9 +3,27 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
+vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3)
+{
+
+}
 void solve()
 {
+    //input
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+
+    int A[n1], B[n2], C[n3];
+    for (int i = 0; i < n1; i++) cin>>A[i];
+    for (int i = 0; i < n2; i++) cin >> A[i];
+    for (int i = 0; i < n3; i++) cin >> A[i];
     
+    //processing
+    vector<int> ans = commonElements(A, B, C, n1, n2, n3);
+    
+    //output
+    for(int i:ans) cout<<i<<" ";
+    cout << endl;
 }
 
 int main() {
