379bb706cd5396aa7652d3ae9e7cbc6597257db9
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-23 18:52:53 +0530

    logic done

--- DSA450/Arrays/19.cpp
+++ DSA450/Arrays/19.cpp
@@ -5,7 +5,23 @@ using namespace std;
 
 vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3)
 {
+    int i, j, k;
+    i = j = k = 0;
+    vector<int> ans;
 
+    while (i < n1 and j<n2 and k<n3)
+    {
+        if(A[i]==B[j] and B[j] == C[k])
+        {
+            ans.push_back(A[i]);
+            i++;
+            j++;
+            k++;
+        }
+        else if(A[i]<B[j]) i++;
+        else if(B[j]<C[k]) j++;
+        else k++;
+    }
 }
 void solve()
 {
