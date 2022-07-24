6a42f6ed660a86b0b73a91da106115629e304053
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-24 14:16:12 +0530

    done

--- Codehelp/Lecture 37/subsets.cpp
+++ Codehelp/Lecture 37/subsets.cpp
@@ -25,3 +25,20 @@ vector<vector<int>> subsets(vector<int> &nums)
 
     return ans;
 }
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n;i++)
+        cin >> arr[i];
+
+    vector<vector<int>> ans = subsets(arr);
+    for(auto i:ans)
+    {
+        for(int j:i) cout<<j<<" ";
+        cout << endl;
+    }
+        
+}
