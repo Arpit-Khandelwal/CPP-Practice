f1ab3135fc051c07ffa9525eb4a816b5096f930a
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-07-25 15:33:30 +0530

    done

--- Codehelp/Lecture 39/PermutationsOfAString.cpp
+++ Codehelp/Lecture 39/PermutationsOfAString.cpp
@@ -1,13 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void solve(string s, vector<string> &ans, int i)
+{
+    if(i>=s.size())
+    {
+        ans.push_back(s);
+        return;
+    }
+    string tempS = s;
+
+    for (int j = i; j < s.length();j++)
+    {
+        swap(tempS[i], tempS[j]);
+        solve(tempS, ans, i + 1);
+        tempS = s;
+    }
+        
+}
+
 int main()
 {
     string s;
     cin >> s;
 
     vector<string> ans;
-    solve();
+    solve(s,ans,0);
 
     for(string i:ans) cout<<i<<" ";
 }
