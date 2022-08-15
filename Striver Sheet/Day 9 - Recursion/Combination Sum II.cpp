a188e2a891669a1236e25bcac7a3e8199917be4a
Arpit-Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2022-08-15 16:16:35 +0530

    done

--- Striver Sheet/Day 9 - Recursion/Combination Sum II.cpp
+++ Striver Sheet/Day 9 - Recursion/Combination Sum II.cpp
@@ -3,23 +3,26 @@ using namespace std;
 #define ll long long
 #define mod 1000000007
 
-void findCombinations(vector<int> &nums, int i, int target, vector<int> temp, vector<vector<int>> &ans)
+void findCombinations(vector<int> &nums, int index, int target, vector<int> &temp, vector<vector<int>> &ans)
 {
-    if (i >= nums.size())
+    if (target == 0)
     {
-        if (target == 0)
-            ans.push_back(temp);
+        ans.push_back(temp);
+
         return;
     }
 
-    if (nums[i] <= target)
+    for (int i = index; i < nums.size(); i++)
     {
+        if (i > index && nums[i] == nums[i - 1])
+            continue;
+        if (nums[i] > target)
+            break;
+
         temp.push_back(nums[i]);
         findCombinations(nums, i + 1, target - nums[i], temp, ans);
         temp.pop_back();
     }
-
-    findCombinations(nums, i + 1, target, temp, ans);
 }
 
 vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
@@ -28,7 +31,14 @@ vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
     vector<int> temp;
     vector<vector<int>> ans;
 
+    sort(candidates.begin(), candidates.end());
     findCombinations(candidates, 0, target, temp, ans);
+
+    // vector<vector<int>> uniqueAns;
+    // for(auto i:ans)
+    // {
+    //     uniqueAns.push_back(i);
+    // }
 
     return ans;
 }
@@ -53,7 +63,7 @@ int main()
     for (auto i : ans)
     {
         for (int j : i)
-            cout << j ;
+            cout << j;
         cout << endl;
     }
 }
